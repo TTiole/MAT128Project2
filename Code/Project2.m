a2c139d3f846d8f9922129d3491cf331e85607f5
Nikos Trembois <ntrembois@gmail.com>
2020-03-03 17:49:55 -0800

    Reduced Parameters

--- Code/Project2.m
+++ Code/Project2.m
@@ -7,6 +7,9 @@ clc; close all;
 n_train = 60000; n_test = 10000;
 [img_train, label_train] = readMNIST('../Data/train-images.idx3-ubyte','../Data/train-labels.idx1-ubyte',n_train,0);
 [img_test , label_test]  = readMNIST('../Data/t10k-images.idx3-ubyte' ,'../Data/t10k-labels.idx1-ubyte' ,n_test,0);
+% flatten data
+trainVector = reshape(img_train,[400,n_train]);
+testVector = reshape(img_test,[400,n_test]);
 
 %% Plot numbers
 % plot_data  = @(A) image(rot90(A')*100);
@@ -19,41 +22,37 @@ n_train = 60000; n_test = 10000;
 %% Testing
 clc
 neurons = [400 8 13 10]; % Define neurons and layers
-% flatten data
-trainVector = reshape(img_train,[400,n_train]);
-testVector = reshape(img_test,[400,n_test]);
 tic
 [err, avgError, prediction, WFinal, correctness, avgCorrectness] = Network(neurons, trainVector(:,1:n_train), testVector(:,1:n_test), label_train(1:n_train), label_test(1:n_test), .2, .05);
 toc
 
 fprintf("The network executed with an average error of %2.2f%% and average correctness of %2.2f%% \n", avgError*100, avgCorrectness*100);
 
-% %% Parameter study
-% neuronPar = {[400 13 7  10];
-%               [400 10 10 10];
-%               [400 3  5   3  10];
-%               [400 20 20 10]};
-%           
-% trainSets = [n_train n_train/2 n_train /10 n_train/60];
-% weightScales = linspace(.5,1.5,3);
-% trainRates = linspace(0.01,1,4);
-% 
-% iter = 0;
-% tic
-% for i = 1:length(neuronPar)
-%     for j = 1:length(trainSets)
-%         for k = 1:length(weightScales)
-%             for l = 1:length(trainRates)
-%                 iter = iter + 1;
-%                 fprintf('On iteration %i of %i',iter,length(neuronPar)*length(trainSets)*length(weightScales)*length(trainRates))
-%                 tic
-%                 Network(neuronPar{i}, trainVector(:,1:trainSets(j)), testVector, label_train(1:trainSets(j)), label_test, weightScales(k), trainRates(l));
-%                 toc
-%             end
-%         end
-%     end
-% end
-% toc
+%% Parameter study
+neuronPar = {[400  13 7  10];
+              [400 10 10 10];
+              [400 4  8   5  10]};
+          
+trainSets = [n_train n_train/2 n_train /10 ]; %n_train/60];
+weightScales = linspace(.5,1.5,3);
+trainRates = linspace(0.01,1,4);
+
+iter = 0;
+tic
+for i = 1:length(neuronPar)
+    for j = 1:length(trainSets)
+        for k = 1:length(weightScales)
+            for l = 1:length(trainRates)
+                iter = iter + 1;
+                fprintf('On iteration %i of %i',iter,length(neuronPar)*length(trainSets)*length(weightScales)*length(trainRates))
+                tic
+                Network(neuronPar{i}, trainVector(:,1:trainSets(j)), testVector, label_train(1:trainSets(j)), label_test, weightScales(k), trainRates(l));
+                toc
+            end
+        end
+    end
+end
+toc
 
 %figure(); hold on;
 %plot(nTrains,error) % plot how the error changes with the number of
